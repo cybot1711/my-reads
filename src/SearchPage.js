60a0e8e32ec1ab50f2c672d8452cc9b59364bfd9
JahilMag <jahil@magneticcreative.co.za>
2017-09-03 10:15:31 +0200

    Feature add: Add book to shelf from Search Page
    Book can now be added from search page and will persist with page reloads.

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -11,41 +11,48 @@ class SearchPage extends Component {
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.textInput.focus()
     }
 
-    getSearchResults(){
+    getSearchResults() {
         const terms = ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'History', 'History', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Program Javascript', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS']
         console.log('getting')
         terms.includes(this.textInput.value) ?
-        BooksAPI.search(this.textInput.value, 10)
-            .then(data => this.setState({data}))
+            BooksAPI.search(this.textInput.value, 10)
+                .then(data => this.setState({data}))
             : alert("Invalid Search term")
     }
 
-    render(){
-        console.log(this.state.data)
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link className="close-search" to="/">Close</Link>
-          <div className="search-books-input-wrapper">
-          
-            <input onBlur={this.getSearchResults.bind(this)} ref={(input) => { this.textInput = input; }} type="text" placeholder="Search by title or author"/>
+    addBookToShelf(id,shelf) {
+        BooksAPI.update(id, shelf)
+        console.log('book added:', id, shelf)
+    }
 
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
-              {
-                  this.state.data.map(item => <Book key={item.id} {...item} />)
-              }
-          </ol>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        console.log(this.state.data)
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+
+                        <input onBlur={this.getSearchResults.bind(this)} ref={(input) => {
+                            this.textInput = input;
+                        }} type="text" placeholder="Search by title or author"/>
+
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {
+                            this.state.data.map(item => <Book key={item.id} {...item} update={this.addBookToShelf.bind(this)} />)
+                        }
+                    </ol>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default SearchPage
