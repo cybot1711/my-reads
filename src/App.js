4a77c6aa22328fd08055876ecac01a2d97e80d4d
JahilMag <jahil@magneticcreative.co.za>
2017-09-04 09:19:58 +0200

    Removed Unused Link import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchPage from './SearchPage'
 import './App.css'
